e3073d1a995b5bcedbe92137c9bb6593b3d5a421
Joe Koberg <jkoberg@gmail.com>
2023-12-04 10:55:27 -0500

    Day 2 Passing

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -2,135 +2,122 @@ package us.jkk.aoc2023
 
 import scala.io.Source
 
-/*
-The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration
-value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit
-and the last digit (in that order) to form a single two-digit number.
+object Day1:
+  /*
+  The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration
+  value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit
+  and the last digit (in that order) to form a single two-digit number.
 
-For example:
+  For example:
 
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+  1abc2
+  pqr3stu8vwx
+  a1b2c3d4e5f
+  treb7uchet
+  In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
-Consider your entire calibration document. What is the sum of all of the calibration values?
-*/
+  Consider your entire calibration document. What is the sum of all of the calibration values?
+  */
 
-def part1(input: Iterator[String]) =
-  input
-    .filter(_.trim.nonEmpty)
-    .map(_.filter(_.isDigit))
-    .map(cs => 10 * cs.head.asDigit + cs.last.asDigit)
-    .sum
-
-
-/*
-Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two,
-three, four, five, six, seven, eight, and nine also count as valid "digits".
-
-Equipped with this new information, you now need to find the real first and last digit on each line. For example:
-
-two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
-
-What is the sum of all of the calibration values?
-*/
-
-val digitRepresentations = Map(
-  "1" -> 1,
-  "2" -> 2,
-  "3" -> 3,
-  "4" -> 4,
-  "5" -> 5,
-  "6" -> 6,
-  "7" -> 7,
-  "8" -> 8,
-  "9" -> 9,
-  "one" -> 1,
-  "two" -> 2,
-  "three" -> 3,
-  "four" -> 4,
-  "five" -> 5,
-  "six" -> 6,
-  "seven" -> 7,
-  "eight" -> 8,
-  "nine" -> 9,
-)
-
-val maxDigitLen = digitRepresentations.keys.map(_.length).max
-
-def scanForDigits(s:String) =
-  for
-    index <- 0 until s.length
-    substring = s.slice(index, index + maxDigitLen)
-    digitValue <- digitRepresentations.collectFirst {
-      case (text, value) if substring.startsWith(text) => value
-    }
-  yield
-    digitValue
+  def part1(input: Iterator[String]): Int =
+    input
+      .filter(_.trim.nonEmpty)
+      .map(_.filter(_.isDigit))
+      .map(cs => 10 * cs.head.asDigit + cs.last.asDigit)
+      .sum
 
 
-def part2(input: Iterator[String]) =
-  val calibrationFactors =
-    for
-      line <- input
-      digitsForLine = scanForDigits(line)
-      if digitsForLine.nonEmpty
-    yield
-      10 * digitsForLine.head + digitsForLine.last
-  calibrationFactors.sum
+  /*
+  Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two,
+  three, four, five, six, seven, eight, and nine also count as valid "digits".
 
+  Equipped with this new information, you now need to find the real first and last digit on each line. For example:
 
+  two1nine
+  eightwothree
+  abcone2threexyz
+  xtwone3four
+  4nineeightseven2
+  zoneight234
+  7pqrstsixteen
+  In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
 
-case class TestCase(
-  fn: Iterator[String] => Int,
-  expected: Int,
-  input: String
-):
-  def run(): Unit =
-    val actual = fn(input.linesIterator)
-    if (actual == expected)
-      println(s"passed. value $actual")
-    else
-      println(s"FAILED! expected $expected, got $actual")
+  What is the sum of all of the calibration values?
+  */
 
-
-val testCases = Seq(
-  TestCase(part1, 142,
-    """
-      1abc2
-      pqr3stu8vwx
-      a1b2c3d4e5f
-      treb7uchet
-    """),
-
-  TestCase(part2, 281,
-    """
-      two1nine
-      eightwothree
-      abcone2threexyz
-      xtwone3four
-      4nineeightseven2
-      zoneight234
-      7pqrstsixteen
-    """)
+  val digitRepresentations: Map[String, Int] = Map(
+    "1" -> 1,
+    "2" -> 2,
+    "3" -> 3,
+    "4" -> 4,
+    "5" -> 5,
+    "6" -> 6,
+    "7" -> 7,
+    "8" -> 8,
+    "9" -> 9,
+    "one" -> 1,
+    "two" -> 2,
+    "three" -> 3,
+    "four" -> 4,
+    "five" -> 5,
+    "six" -> 6,
+    "seven" -> 7,
+    "eight" -> 8,
+    "nine" -> 9,
   )
 
+  val maxDigitLen: Int = digitRepresentations.keys.map(_.length).max
 
-@main
-def runTests(): Unit =
-  testCases.foreach(_.run())
+  def scanForDigits(s:String): Seq[Int] =
+    for
+      index <- 0 until s.length
+      substring = s.slice(index, index + maxDigitLen)
+      digitValue <- digitRepresentations.collectFirst {
+        case (text, value) if substring.startsWith(text) => value
+      }
+    yield
+      digitValue
 
 
-@main
-def main(): Unit =
-  println(s"Part 1: ${part1(Source.fromResource("day1input.txt").getLines())}")
-  println(s"Part 2: ${part2(Source.fromResource("day1input.txt").getLines())}")
+  def part2(input: Iterator[String]): Int =
+    val calibrationFactors =
+      for
+        line <- input
+        digitsForLine = scanForDigits(line)
+        if digitsForLine.nonEmpty
+      yield
+        10 * digitsForLine.head + digitsForLine.last
+    calibrationFactors.sum
+
+
+  val testCases: Seq[TestCase] = Seq(
+    TestCase(part1, 142,
+      """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+      """),
+
+    TestCase(part2, 281,
+      """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+      """)
+    )
+
+
+  @main
+  def runTests(): Unit =
+    testCases.foreach(_.run())
+
+
+  @main
+  def main(): Unit =
+    println(s"Part 1: ${part1(Source.fromResource("day1input.txt").getLines())}")
+    println(s"Part 2: ${part2(Source.fromResource("day1input.txt").getLines())}")
---
+++ src/main/scala/Day2.scala
@@ -0,0 +1,126 @@
+package us.jkk.aoc2023
+
+import scala.io.Source
+
+object Day2:
+  /*
+  To get information, once a bag has been loaded with cubes, the Elf will reach into the bag,
+  grab a handful of random cubes, show them to you, and then put them back in the bag. He'll
+  do this a few times per game.
+
+  You play several games and record the information from each game (your puzzle input). Each
+  game is listed with its ID number (like the 11 in Game 11: ...) followed by a semicolon-separated
+  list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
+
+  For example, the record of a few games might look like this:
+
+  Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+  In game 1, three sets of cubes are revealed from the bag (and then put back again).
+  The first set is 3 blue cubes and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes;
+  the third set is only 2 green cubes.
+
+  The Elf would first like to know which games would have been possible if the bag contained
+  only 12 red cubes, 13 green cubes, and 14 blue cubes?
+   */
+
+  case class Draw(red: Int, blue: Int, green: Int)
+
+  case class Game(id: Int, draws: Seq[Draw])
+
+  def parseDraw(s:String): Draw =
+    val drawData =
+      s.split(",")
+        .map(_.trim)
+        .map {
+          case s"${count} ${color}" =>
+            color -> count.toInt
+          }
+        .toMap
+    Draw(drawData.getOrElse("red", 0), drawData.getOrElse("blue", 0), drawData.getOrElse("green", 0))
+
+  def parseGameLine(s:String): Option[Game] =
+    s match
+      case s"Game ${idText}: ${allDrawsText}" =>
+        val draws = allDrawsText.split(";").map(parseDraw)
+        Some(Game(idText.toInt, draws))
+      case _ => None
+
+  def parseInput(input:Iterator[String]): Iterator[Game] =
+    for
+      line <- input
+      game <- parseGameLine(line)
+    yield
+      game
+
+
+  def part1(input: Iterator[String]): Int =
+    val validGameIds =
+      parseInput(input)
+        .filter(_.draws.forall(d => d.red <= 12 && d.green <= 13 && d.blue <= 14))
+        .map(_.id)
+    validGameIds.sum
+
+
+  /*
+  --- Part Two ---
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+   */
+
+  def maxOf(game: Game, fn: Draw => Int): Int =
+    game.draws.map(fn).maxOption.getOrElse(0)
+
+  def part2(input: Iterator[String]): Int =
+    val gamePowers =
+      parseInput(input)
+        .map(g => {
+          val redMax = maxOf(g, _.red)
+          val blueMax = maxOf(g, _.blue)
+          val greenMax = maxOf(g, _.green)
+          Seq(redMax, blueMax, greenMax).product
+        })
+    gamePowers.sum
+
+
+  val testCaseInput = """
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+
+  val testCases: Seq[TestCase] = Seq(
+    TestCase(part1, 8, testCaseInput),
+    TestCase(part2, 2286, testCaseInput)
+  )
+
+  @main
+  def runDay2Tests(): Unit = testCases.foreach(_.run())
+
+  @main
+  def runDay2Input(): Unit =
+    println(s"Day 2 Part 1: ${part1(Source.fromResource("day2input.txt").getLines())}")
+    println(s"Day 2 Part 2: ${part2(Source.fromResource("day2input.txt").getLines())}")
+
